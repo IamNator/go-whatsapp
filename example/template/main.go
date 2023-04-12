0ccac0d4d8a883978a5684dafb5a36d4688b73c5
Nator Verinumbe <natverior1@gmail.com>
2023-04-12 17:19:00 +0100

    added docs for examples

--- example/template/main.go
+++ example/template/main.go
@@ -1,3 +1,35 @@
+//package main
+//
+//import (
+//	"context"
+//	"fmt"
+//
+//	whatsapp "github.com/iamNator/go-whatsapp"
+//	"github.com/iamNator/go-whatsapp/template"
+//)
+//
+//func main() {
+//	client := whatsapp.New("metaAppId", "metaAppAccessToken", whatsapp.V15)
+//
+//	// prepare the payload
+//
+//	//build the template
+//	tmpl := template.New("templateName", template.EN_US).AddHeader("header").
+//		AddBody("body").
+//		AddBody("body").
+//		AddBody("body").
+//		Done()
+//
+//	// send the request
+//	response, errResponse, er := client.SendTemplate(context.Background(), "2349045057268", tmpl)
+//	if er != nil {
+//		fmt.Println("error: ", er.Error())
+//		return
+//	}
+//
+//	fmt.Println("Err: ", errResponse, "\nResponse: ", response)
+//}
+
 package main
 
 import (
--- example/text/main.go
+++ example/text/main.go
@@ -1,3 +1,25 @@
+//package main
+//
+//import (
+//	"context"
+//	"fmt"
+//
+//	whatsapp "github.com/iamNator/go-whatsapp"
+//)
+//
+//func main() {
+//	client := whatsapp.New("metaAppId", "metaAppAccessToken", whatsapp.V15)
+//
+//	// send the request
+//	response, errResponse, er := client.SendText(context.Background(), "2349045057268", "Hello World")
+//	if er != nil {
+//		fmt.Println("error: ", er.Error())
+//		return
+//	}
+//
+//	fmt.Println("Err: ", errResponse, "\nResponse: ", response)
+//}
+
 package main
 
 import (
